55524bc1945b01386b1f5ac47118393caace7881
PeeperInTheSpace <lollof94@gmail.com>
2022-11-07 18:47:40 +0100

    Completed!

---
+++ database/seeds/TrainsTableSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+use Faker\Generator as Faker;
+
+use App\Train;
+
+class TrainsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        //
+        $tempCode ='TT02030';
+        for($i = 0; $i < 100; $i++){
+
+            $train = new Train();
+            $train->azienda = $faker->name();
+            $train->stazione_partenza = $faker->city();
+            $train->stazione_arrivo = $faker->city();
+            $train->orario_partenza = $faker->time();
+            $train->orario_arrivo = $faker->time();
+            $randomDate = $faker->date();
+            $train->data_partenza = $faker->$randomDate;
+            $train->data_arrivo = $faker->$randomDate;
+            $train->codice_treno = $tempCode . $i;
+            $train->save();
+
+        }
+
+    }
+}
